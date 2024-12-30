c6cf5a72e76819885b91c1ed7f4baf6e18a63769
Final-year2025 <finalezproject@gmail.com>
2024-12-30 23:01:35 +0530

    scroll button added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import "./App.css"
 import Banner from './Components/Banner/banner';
-import {action,originals,comedy} from './url'
+import { action, originals, comedy } from './url'
 import RawPost from './Components/RawPost/RawPost';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Banner/>
-      <RawPost url={originals}  title='Netflix Originals' />
-      <RawPost url={action}  title='Actions' isSmall />
-      <RawPost url={comedy}  title='ComedyMovies' isSmall />
+      <Navbar />
+      <Banner />
+      <div className="row-container">
+        <RawPost url={originals} title="Netflix Originals" />
+      </div>
+      <div className="row-container">
+        <RawPost url={action} title="Actions" isSmall />
+      </div>
+      <div className="row-container">
+        <RawPost url={comedy} title="ComedyMovies" isSmall />
+      </div>
     </div>
   );
 }
--- src/Components/RawPost/RawPost.js
+++ src/Components/RawPost/RawPost.js
@@ -1,51 +1,121 @@
-import React,{useEffect,useState} from 'react'
-import Youtube from 'react-youtube'
-import './RawPost.css'
-import {imageUrl,API_KEY} from '../Constants/constants'
-import axios from '../../axios'
+import React, { useEffect, useState, useRef } from "react";
+import Youtube from "react-youtube";
+import { imageUrl, API_KEY } from "../Constants/constants";
+import axios from "../../axios";
+import "./RawPost.css";
+
 function RawPost(props) {
-  const [movies, setMovies] = useState([])
-  const [urlId,setUrlId] = useState('')
-  useEffect(()=>{
-    axios.get(props.url).then(response =>{
-      console.log(response.data)
-      setMovies(response.data.results)
-    }).catch(err=>{
-      alert('Network error')
-    })
-  },[])
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
+  const postersRef = useRef(null);
+
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        alert("Network error");
+      });
+  }, [props.url]);
+
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
-    
   };
-  const handleMovie = (id)=>{
-    console.log(id)
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response =>{
-      if(response.data.results[0]){
-        setUrlId(response.data.results[0])
-      }else{
-        console.log("Array empty")
-      }
 
-    })
+  const handleMovie = (id) => {
+    console.log(id);
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results[0]) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("Array empty");
+        }
+      });
+  };
 
-  }
+  const scrollLeft = () => {
+    postersRef.current.scrollBy({
+      left: -500,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollRight = () => {
+    postersRef.current.scrollBy({
+      left: 500,
+      behavior: "smooth",
+    });
+  };
+
+  // Dynamic button styles based on props.title
+  const getButtonStyles = (type) => {
+    const baseStyles = {
+      position: "absolute",
+      top: "50%",
+      backgroundColor: "rgba(0, 0, 0, 0.6)",
+      color: "white",
+      border: "none",
+      padding: "10px 20px",
+      cursor: "pointer",
+      zIndex: 10,
+      fontSize: "18px",
+      borderRadius: "5px",
+      transform: "translateY(-50%)",
+      opacity: 0.7,
+      transition: "all 0.3s ease-in-out",
+    };
+
+    const sectionStyles = {
+      "Netflix Originals": { backgroundColor: "#e50914" }, // Red for Netflix Originals
+      Action: { backgroundColor: "#0074e4" }, // Blue for Action
+      "Comedy Movies": { backgroundColor: "#f5c518" }, // Yellow for Comedy
+    };
+
+    const positionStyles =
+      type === "left"
+        ? { left: "10px" }
+        : { right: "10px" }; // Position left or right based on the button type
+
+    return { ...baseStyles, ...sectionStyles[props.title], ...positionStyles };
+  };
+
   return (
-    <div className='row'>
-        <h2>{props.title}</h2>
-        <div className="posters">
-          {movies.map((obj)=>
-            <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-            )}
-        </div>
-      { urlId &&  <Youtube opts={opts} videoId={urlId.key}/> }
+    <div className="row" style={{ position: "relative" }}>
+      <h2>{props.title}</h2>
+      <button
+        style={getButtonStyles("left")}
+        onClick={scrollLeft}
+      >
+        {"<"}
+      </button>
+      <button
+        style={getButtonStyles("right")}
+        onClick={scrollRight}
+      >
+        {">"}
+      </button>
+      <div className="posters" ref={postersRef}>
+        {movies.map((obj) => (
+          <img
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            src={`${imageUrl + obj.backdrop_path}`}
+            alt="poster"
+          />
+        ))}
+      </div>
+      {urlId && <Youtube opts={opts} videoId={urlId.key} />}
     </div>
-  )
+  );
 }
 
-export default RawPost
+export default RawPost;
